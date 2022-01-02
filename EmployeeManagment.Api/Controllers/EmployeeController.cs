849cbc7cfad03ee39231d2443b4fa3644b83d145
abdulrahmannady <abdulrahmann@silverkeytech.com>
2022-01-02 13:36:53 +0200

    Create an Api to return list of employees

---
+++ EmployeeManagment.Api/Controllers/EmployeeController.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeManagment.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EmployeeManagment.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> GetEmployees()
+        {
+            try
+            {
+                return Ok(await EmployeeRepsitory.GetEmployees());
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error returning Data");
+            }
+
+        }
+    }
+}
--- EmployeeManagment.Web/Pages/EmployeeListBase.cs
+++ EmployeeManagment.Web/Pages/EmployeeListBase.cs
@@ -8,43 +8,9 @@ namespace EmployeeManagment.Web.Pages
 {
     public class EmployeeListBase : ComponentBase
     {
-        public IEnumerable<Employee> Employees { get; set; }
-
         protected override async Task OnInitializedAsync()
         {
-            await Task.Run(LoadEmployees);
-            //return base.OnInitializedAsync();
-        }
-
-        private void LoadEmployees()
-        {
-            System.Threading.Thread.Sleep(3000);
-            Employees = new List<Employee>()
-            {
-                new Employee()
-                {
-                    EmployeeId = 1,
-                    FirstName = "Nady",
-                    LastName = "Ahmed",
-                    Email = "Ahmed@gmail.com",
-                    DateOfBirth = new DateTime(1980, 10, 5),
-                    Gender = Gender.Male,
-                    Department = new Department { DepartmentId = 1, DepartmentName = "IT" },
-                    PhotoPath = "images/stonks.jpg"
-                },
-                new Employee()
-                {
-                    EmployeeId = 2,
-                    FirstName = "Nour",
-                    LastName = "Sayed",
-                    Email = "Nour@gmail.com",
-                    DateOfBirth = new DateTime(1981, 12, 22),
-                    Gender = Gender.Female,
-                    Department = new Department { DepartmentId = 2, DepartmentName = "HR" },
-                    PhotoPath = "images/female.jpg"
-                }
-            };
-        }
+        }   
     }
 }
 
