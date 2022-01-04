e6c92cab4a50f992645689ede19761fd0f9bcd66
abdulrahmannady <abdulrahmann@silverkeytech.com>
2022-01-04 18:25:44 +0200

    add details component

---
+++ EmployeeManagment.Web/Pages/EmployeeDetailsBase.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeManagment.Web.Services;
+using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
+using Views.DtoClasses;
+
+namespace EmployeeManagment.Web.Pages
+{
+    public class EmployeeDetailsBase : ComponentBase
+    {
+        [Inject]
+        public EmployeeService EmployeeService { get; set; }
+
+        [Parameter]
+        public string Id { get; set; }
+
+        public EmployeeView Employee { get; set; } = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+    }
+}
