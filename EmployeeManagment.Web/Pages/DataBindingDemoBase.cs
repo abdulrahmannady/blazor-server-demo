e946379f4edffc9bc2df9221575aac8f1fe1dac3
abdulrahmannady <abdulrahmann@silverkeytech.com>
2022-01-04 21:54:06 +0200

    practice data binding one way from class to view and two way from view to class and vice versa

---
+++ EmployeeManagment.Web/Pages/DataBindingDemoBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagment.Web.Pages
+{
+    public class DataBindingDemoBase : ComponentBase
+    {
+        public string Name { get; set; } = "Nady";
+        public string Gender { get; set; } = "Male";
+        public string Colour { get; set; } = "background-color:White";
+    }
+}
--- EmployeeManagment.Web/Pages/EmployeeDetailsBase.cs
+++ EmployeeManagment.Web/Pages/EmployeeDetailsBase.cs
@@ -14,13 +14,29 @@ namespace EmployeeManagment.Web.Pages
         [Parameter]
         public string Id { get; set; }
 
-        protected string coordinates { get; set; }
+        protected string Coordinates { get; set; }
+        protected string ButtonText { get; set; } = "Hide Footer";
+        protected string CssClass { get; set; } = null;
 
         public EmployeeView Employee { get; set; } = new();
 
         protected override async Task OnInitializedAsync()
         {
             Employee = await EmployeeService.GetEmployee(int.Parse(Id));
+        }
+
+        protected void Button_Click()
+        {
+            if (ButtonText == "Hide Footer")
+            {
+                ButtonText = "Show Footer";
+                CssClass = "hide-footer";
+            }
+            else
+            {
+                CssClass = null;
+                ButtonText = "Hide Footer";
+            }
         }
     }
 }
