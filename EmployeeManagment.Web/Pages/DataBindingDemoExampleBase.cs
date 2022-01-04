bae9da58706c9fbadbe7ed139491d9df1157c256
abdulrahmannady <abdulrahmann@silverkeytech.com>
2022-01-04 22:49:08 +0200

    data binding demo example

---
+++ EmployeeManagment.Web/Pages/DataBindingDemoExampleBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace EmployeeManagment.Web.Pages
+{
+    public class DataBindingDemoExampleBase : ComponentBase
+    {
+        protected string TextArea { get; set; } = string.Empty;
+
+        protected int Counter { get; set; }
+    }
+}
